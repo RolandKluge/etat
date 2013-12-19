64b9b9bf61ef355dd992cd33c6cf80344721c5a3
Roland Kluge <roland.kluge@gmx.de>
2013-12-19 09:17:41 +0100

    Extend books overview  * Add images to books overview  * Create link to editbook.php

---
+++ editbook.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Buch anlegen/bearbeiten</title>
+    </head>
+    <body>
+        <h1>Buch anlegen/bearbeiten</h1>
+        <form>
+            <label for="name">Name</label>
+            <input type="text" size="40"></input>
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -14,22 +14,30 @@ $books = $bookDao->getAll();
     <head>
         <meta charset="UTF-8">
         <title>Overview of Books</title>
+        <link rel="stylesheet" href="static/css/stylesheet.css">
     </head>
     <body>
         <h2>Books</h2>
-        <ul>
+        <ul id="booksList">
             <?php
             foreach ($books as $book) {
                 $name = $book->getName();
                 ?>
                 <li>
                     <a href="./viewbook.php?book=<?php echo $name ?>">
-                        <?php echo $book->getId() . ": " . $book->getName() ?>
+                        <img src="static/images/book.png" width="48" height="48"/>
+                        <div class="bookId"><?php echo $book->getId(); ?></div>:<div class="bookName"><?php echo $book->getName(); ?></div>
                     </a>
                 </li>
                 <?php
             }
             ?>
+            <li>
+                <a href="./editbook.php?action=create">
+                    <img src="static/images/book_new.png" width="48" height="48"/>
+                    <div class="bookName">Neues Buch anlegen...</div>
+                </a>
+            </li>
         </ul>
     </body>
 </html>
