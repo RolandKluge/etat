ca79e03e22b6d119957c308f95897daf3a5d346a
Roland Kluge <roland.kluge@gmx.de>
2013-12-26 08:25:56 +0100

    Current database format

--- sql/etat.sql
+++ sql/etat.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 12, 2013 at 07:34 AM
+-- Generation Time: Dec 26, 2013 at 08:06 AM
 -- Server version: 5.5.34-0ubuntu0.13.10.1
--- PHP Version: 5.5.3-1ubuntu2
+-- PHP Version: 5.5.3-1ubuntu2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,18 +26,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `books`
 --
 
+DROP TABLE IF EXISTS `books`;
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
   `description` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`id`, `description`) VALUES
-(1, 'Testbuch');
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=37 ;
 
 -- --------------------------------------------------------
 
@@ -45,14 +41,20 @@ INSERT INTO `books` (`id`, `description`) VALUES
 -- Table structure for table `entries`
 --
 
+DROP TABLE IF EXISTS `entries`;
 CREATE TABLE IF NOT EXISTS `entries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `amount` decimal(10,2) NOT NULL,
   `date` date NOT NULL,
+  `description` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `book_id` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `id_2` (`id`),
+  KEY `book_id` (`book_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
 
@@ -60,11 +62,13 @@ CREATE TABLE IF NOT EXISTS `entries` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `is_real` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=28 ;
 
 -- --------------------------------------------------------
 
@@ -72,12 +76,15 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `users_to_books`
 --
 
+DROP TABLE IF EXISTS `users_to_books`;
 CREATE TABLE IF NOT EXISTS `users_to_books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`,`book_id`),
   KEY `book_id` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=35 ;
 
 --
 -- Constraints for dumped tables
@@ -87,7 +94,8 @@ CREATE TABLE IF NOT EXISTS `users_to_books` (
 -- Constraints for table `entries`
 --
 ALTER TABLE `entries`
-  ADD CONSTRAINT `entry_to_book` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `entry_to_book` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `entry_to_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users_to_books`
